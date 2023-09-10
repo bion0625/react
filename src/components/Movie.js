d0e6d4eeaa3105960302c71ec9336272422beb35
이의정 <stxtory@gmail.com>
2023-09-10 20:13:35 +0900

    summary text size

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,7 +8,7 @@ const Movie = ({id, coverImg, title, summary, genres}) => {
             <h2>
                 <Link to={`/movie/${id}`}>{title}</Link>
             </h2>
-            <p>{summary}</p>
+            <p>{summary.length > 200 ? `${summary.slice(0,200)}...` : summary}</p>
             <ul>{genres.map(g => <li key={g}>{g}</li>)}</ul>
         </div>
     );
