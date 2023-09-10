c9241f4445681a28792b30b6c68500b626b1003b
이의정 <stxtory@gmail.com>
2023-09-10 10:27:36 +0900

    code challenge

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const insertMoney = (event) => {
+    setMoney(event.target.value);
+    getCount();
+  }
+
+  const getCount = () => {
+    setCount(Math.round(money / document.querySelector('#coin').value));
+  }
+
   useEffect(()=>{
     fetch("https://api.coinpaprika.com/v1/tickers")
     .then(responce => responce.json())
@@ -18,13 +30,17 @@ function App() {
         loading ? (
           <strong>Loading...</strong>
           ) : (
-          <select>
+          <select id="coin" onChange={getCount}>
           {coins.map((coin) => 
-            <option key={coin.id}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</option>
+            <option value={coin.quotes.USD.price} key={coin.id}>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</option>
           )}
           </select>
         )
       }
+      <div>
+      <span>$</span><input type="number" placeholder="write the money (USD)" onChange={insertMoney} value={money} /><span>USD</span>
+      </div>
+      <span>you can buy {count}</span>
     </div>
   );
 }
